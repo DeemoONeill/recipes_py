7e4329d55408e32daa86658a5d5386548b25d4f7
Matthew O'Neill <oneill_91@hotmail.co.uk>
2023-09-03 11:32:57 +0100

    added portions to the Json
    added a recipe class to the src file and changed the collection of the recipes
    updated the testing file to work with the new collection and class

--- src/recipes.py
+++ src/recipes.py
@@ -5,8 +5,26 @@ import json
 import pyinputplus as pyin
 
 
+class Recipe:
+    def __init__(self, name, ingredients: dict[str, int], portions):
+        self.name = name
+
+        # we want to normalise this to get the ingredients for a single portion
+        normalised_ingredients = {
+            ingredient: quantity / portions
+            for ingredient, quantity in ingredients.items()
+        }
+
+        # TODO: add ingredients class with rules for how to divide them.
+        # e.g. eggs / any number should give a minimum of 1
+        # pasta can be divisible by any number
+        self.ingredients = normalised_ingredients
+
+        self.portions = portions
+
+
 # we want to be able to select multiple recipes
-def recipe_selection(recipes: dict):
+def recipe_selection(recipes: dict[str, Recipe]):
     list_of_wanted_recipes = []
     # input
     valid_choices = list(recipes.keys())
@@ -29,7 +47,7 @@ def recipe_selection(recipes: dict):
 # we want to aggregate all the ingredients from those, to make a shopping list
 
 
-def make_shopping_list(selections: list, recipes: dict) -> dict:
+def make_shopping_list(selections: list, recipes: dict[str, Recipe]) -> dict:
     """This function takes a list of recipe selections and
     returns a shopping list
     """
@@ -37,7 +55,7 @@ def make_shopping_list(selections: list, recipes: dict) -> dict:
     shopping_list = {}
     for key in selections:
         # loop through the ingredients in each recipe
-        for ingredient, quantity in recipes[key].items():
+        for ingredient, quantity in recipes[key].ingredients.items():
             # loop through each ingredient in each list
             if ingredient in shopping_list:
                 # if the ingredient has shown up before, add 1 to it
@@ -51,7 +69,9 @@ def make_shopping_list(selections: list, recipes: dict) -> dict:
 
 def main():
     with open("recipes.json") as file_handle:
-        recipes = json.loads(file_handle.read())
+        recipes: dict[str, dict] = json.loads(file_handle.read())
+
+    recipes = {key: Recipe(name=key, **value) for key, value in recipes.items()}
 
     selections = recipe_selection(recipes)
     shopping_list = make_shopping_list(selections, recipes)
