a744a1d319fa3f6252ab48928293845b85ceaf9d
Christopher O'Neill <rustyoneill88@hotmail.com>
2023-11-12 20:59:17 +0000

    added the HTML doc to it to get it rendering our form

--- webserver.py
+++ webserver.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+import template_me
 
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/", response_class=HTMLResponse)
 async def root():
-    return {"Message": "Hello World"}
+    return template_me.template.render(recipes=template_me.recipes)
+
+
+@app.get("/selected/{selected_recipe}")
+async def read_item(selected_recipe: str):
+    return {"selected_recipe": selected_recipe}
